fe06df1f503cf4526107c6a605a4480e712db80f
GeromeYabut <gerome.yabut@gmail.com>
2018-04-23 11:45:58 -0700

    Added photo upload

--- sdflask/app/__init__.py
+++ sdflask/app/__init__.py
@@ -1,3 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template, request
 app = Flask(__name__)
 from app import routes
+import os
+
+
+
+UPLOAD_FOLDER = os.path.basename('uploads')
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    file = request.files['image']
+    f = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+    
+    # add your custom code to check that the uploaded file is a valid image and not a malicious file (out-of-scope for this post)
+    file.save(f)
+    
+    return render_template('camera.html')
--- sdflask/sdflask.py
+++ sdflask/sdflask.py
@@ -1,2 +1,23 @@
 from app import app
 
+"""
+import os
+from flask import Flask, render_template, request
+
+
+app = Flask(__name__)
+
+UPLOAD_FOLDER = os.path.basename('uploads')
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    file = request.files['image']
+    f = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+    
+    # add your custom code to check that the uploaded file is a valid image and not a malicious file (out-of-scope for this post)
+    file.save(f)
+    
+    return render_template('camera.html')
+"""
